0043cf95ad59a911242735105cd33f327fc0a006
Lewis Penman <71828222+LP404@users.noreply.github.com>
2023-11-15 13:13:16 +0000

    Completed Second Prelim Task

---
+++ Exercises2023/Ex1_2/Assignment/Prelim/VectorMagFinderWithInput.cxx
@@ -0,0 +1,27 @@
+// LP 15/11/23 Adding some numbers
+
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+//Function for squaring a float
+int squareNum(float valIn); //prototype
+
+int squareNum(float valIn){ //def
+    return (valIn*valIn);
+}
+
+int main(){
+
+    //Declare variables
+    float x = 2.3;
+    float y = 4.5;
+
+    //Do the monster math according to some vector properties
+    float mag = sqrt((squareNum(x) * squareNum(y)));
+
+    //Print
+    std::cout << mag << std::endl;
+    return 0;
+
+}
