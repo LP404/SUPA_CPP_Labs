773b7bb8768895fd91561ad2cca2ebccf40f9be8
Lewis Penman <71828222+LP404@users.noreply.github.com>
2023-11-21 15:39:55 +0000

    Working on task 3

--- Exercises2023/Ex1_2/Assignment/Main/AnalyseData.cxx
+++ Exercises2023/Ex1_2/Assignment/Main/AnalyseData.cxx
@@ -6,16 +6,18 @@
 #include <string>
 #include <vector>
 
-int main(){
+/// Function will read a two colum txt file, it will also discard the first line
+float ReafFunc(str fileName){
 
-std::ifstream myInput("input2D_float.txt");
+std::ifstream myInput(fileName);
 
 if(!myInput.is_open()){
     std::cout<<"File is not open"<<std::endl;
     return 0;
 }
 
-float x,y;
+float xHold,yHold;
+std::vector<float> x,y;
 std::string strHold;
 std::string line;
 
@@ -28,10 +30,59 @@ while(getline(myInput,line)){
     }
     std::stringstream findLine(line);
     getline(findLine,strHold,',');
-    x = std::stof(strHold);
+    x.push_back(std::stof(strHold));
     getline(findLine,strHold,',');
-    y = std::stof(strHold);
-    std::cout<<x<<" "<<y<<std::endl;
+    y.push_back(std::stof(strHold));
+}
+
+
+    return x,y;
+}
+
+
+str printFunc(float x, float y, int n){
+
+///We can safely assume in this case that each x will have a corresponding y value
+///If not then I have to do better input checking
+
+if (n > x.size() and n < 5){
+    std::cout<<"Warning! Number of values selected is greater than number of values avaliable"<<std::endl;
+    std::cout<<"Printing all avliable lines"<<std::endl;
+
+    for (int i = 0; i < n; i++){
+
+        std::cout<<"Printing all avliable lines"<< x.[0] <<std::endl;
+        std::cout<<"Printing all avliable lines"<<std::endl;
+
+    }
+
+}
+
+else if(n > x.size() and n >= 5){
+    std::cout<<"Warning! Number of values selected is greater than number of values avaliable"<<std::endl;
+    std::cout<<"Printing first five lines."<<std::endl;
+
+
+
+
+}
+
+else{
+
+
+
+
+}
+    std::cout<<xHold<<" "<<yHold<<std::endl;
+}
+
+
+int main(){
+
+"input2D_float.txt"
+
+
+
 }
 
 myInput.close();
