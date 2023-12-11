b6f2c010a3f302be379deab15bbd58b9c0a7d5b0
Lewis Penman <71828222+LP404@users.noreply.github.com>
2023-12-11 22:55:38 +0000

    Added comments to the test.cxx

--- Exercises2023/Ex3_4/Test.cxx
+++ Exercises2023/Ex3_4/Test.cxx
@@ -8,15 +8,21 @@
 ///The negative Crystal ball function only plots right roughly half of the time and I don't know why. If I had more time I'd look into it properly
 ///Just run it again if it doesn't plot right
 
+///It was found that a Cauchy-Lorentz Distribution with a gamma of 0.75 and a x0 of 0, was the correct one for the data.
+
 int main(){
 
+    ///Imports the generated values from the txt
     std::vector<double> importVals;
     Ex34Functions rfunc;
     importVals = rfunc.ReadFunc("Outputs/data/MysteryData13120.txt");
 
+    ///Figues out the max and min values
     std::vector<double>::iterator m_RMin = std::min_element(importVals.begin(), importVals.end());
     std::vector<double>::iterator m_RMax = std::max_element(importVals.begin(), importVals.end());
 
+    ///Calls/constructs the relecent class, with appropritate input paramaters/ distributions selected
+    ///This is true for all of the next four calls
     FiniteFunction func(*m_RMin,*m_RMax,"Output");
     func.plotFunction();
     func.plotData(importVals,100);
@@ -49,35 +55,43 @@ int main(){
     crfunc.printInfo();
 
 
+    ///Need to declate vectors to hold the linearly spaced array
     std::vector<double> GaussMet;
     std::vector<double> CauchLorMet;
     std::vector<double> CrystalMet;
     
+    ///Pickes a nice even number for our samples
     int samSize = 100000;
 
+    ///Assigns values to these vectors
     GaussMet = gfunc.VectorMaker(samSize);
     CauchLorMet = cfunc.VectorMaker(samSize);
     CrystalMet = crfunc.VectorMaker(samSize);
 
+    ///Will now put each x values in the vector to get f(x) for each distribution
     for (int i=0; i<samSize; i++) {
         GaussMet[i] = gfunc.callFunction(GaussMet[i]);
         CauchLorMet[i] = cfunc.callFunction(CauchLorMet[i]);
         CrystalMet[i] = crfunc.callFunction(CrystalMet[i]);
     }
 
+    ///Performs the metropolis alogithim on the data to get the random samples
    for (int i=0; i<samSize; i++) {
         GaussMet[i+1] = gfunc.Metropolis(GaussMet[i],0,0.25);
         CauchLorMet[i+1] = cfunc.Metropolis(CauchLorMet[i],0,0.25);
         CrystalMet[i+1] = crfunc.Metropolis(CrystalMet[i],0,3);
     }
 
+    ///Plots the data, not all of it.
     gfunc.plotData(GaussMet,100,false);
     cfunc.plotData(CauchLorMet,100,false);
     crfunc.plotData(CrystalMet,100,false);
 
     //Bonus task was completed but has been left commented out as it increases runtime by alot
+    //Does not report a fully accurate Pi value
     //Ex34Functions piFunc ;
     //piFunc.PiFinder(1.0,100000);
 
+    ///Function has to return something
     return 0;
 }
